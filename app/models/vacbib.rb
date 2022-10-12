9d96f3e754449f7fbd87e8c811cc7caad5633fa7
kimvanrompay <kim@therubylab.com>
2022-10-12 07:39:37 +0000

    creation 404 page

--- app/models/vacbib.rb
+++ app/models/vacbib.rb
@@ -1,2 +1,3 @@
 class Vacbib < ApplicationRecord
+    has_rich_text :body
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,13 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"
+pin "json", to: "https://ga.jspm.io/npm:json@11.0.0/lib/json.js"
+pin "assert", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.27/nodelibs/browser/assert.js"
+pin "events", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.27/nodelibs/browser/events.js"
+pin "fs", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.27/nodelibs/browser/fs.js"
+pin "path", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.27/nodelibs/browser/path.js"
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.27/nodelibs/browser/process-production.js"
+pin "util", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.27/nodelibs/browser/util.js"
+pin "vm", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.27/nodelibs/browser/vm.js"
